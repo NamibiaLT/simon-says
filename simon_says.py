fa90ffdc27b00bdfeb3315918b47f259d7452001
Namibia Torres <NamibiaLT@gmail.com>
2020-04-02 17:11:21 -0700

    revamp simon says code

--- simon_says.py
+++ simon_says.py
@@ -3,20 +3,11 @@ import time
 
 
 def introduction():
-    game_prompts = [
-        "Simon says enter 1",
-        "enter 1",
-        "Simon says enter 2",
-        "enter 2",
-        "Simon says enter 3",
-        "enter 3"
-    ]
-
-    print("Simon Says is a game where the user will type in their command line commands that the Simon app instructs, starting with 'Simon Says' followed by the command")
+    print("Simon Says is a game where the user will type in their command line commands that the Simon app instructs, starting with 'Simon says' followed by the command")
     time.sleep(1)
-    print("If the command does not start with Simon says hit the Space Bar")
-    simonSays(game_prompts)
-
+    print("If the command does not start with Simon says hit the Space Bar and then hit ENTER")
+    time.sleep(1)
+    # simonSays(game_prompts)
 
 # Conditions to keep game running:
 # The user types the correct thing
@@ -24,13 +15,56 @@ def introduction():
 # Condition to end game:
 # User types the wrong thing
 # User doesn't type  SPACE BAR if the command doesn't start with Simon says
+
+
+game_prompts = [
+    "Simon says enter 1",
+    "enter 1",
+    "Simon says enter 2",
+    "enter 2",
+    "Simon says enter 3",
+    "enter 3"
+]
+
+
 def simonSays(game_prompts):
-    pick = random.choice(game_prompts)
-    used_picks = []
-    for command in game_prompts:
-        while not pick.startswith("Simon says ") and input() != " ":
+    random.shuffle(game_prompts)
+    pick = game_prompts[0]
+
+    while pick:
+        print(pick)
+        if pick.startswith("Simon says ") and input() == int(pick[-1]):
+            print("Success!")
+            game_prompts.pop(0)
+
+        elif not pick.startswith("Simon says ") and input() == " ":
+            game_prompts.pop(0)
+
+        elif not pick.startswith("Simon says ") and input() != " ":
             print("FAILURE! You have failed the game")
-        used_picks.append(pick)
+            break
+
+        elif game_prompts == []:
+            print("You win the game!")
+            break
+
+    # while pick.startswith("Simon says ") and input() == int(pick[-1]):
+    #     print("Success!")
+    #     game_prompts.pop(0)
+    #     if game_prompts == []:
+    #         print("You win the game!")
+    #         break
+
+    # while not pick.startswith("Simon says ") and input() == " ":
+    #     game_prompts.pop(0)
+    #     if game_prompts == []:
+    #         print("You win the game!")
+    #         break
+
+    # while not pick.startswith("Simon says ") and input() != " ":
+    #     print("FAILURE! You have failed the game")
+    #     break
 
 
 introduction()
+simonSays(game_prompts)
